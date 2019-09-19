2264a406509dd15d98b8c2e9ec9afb16b65bdfc4
Sipun <Sipun@DESKTOP-QVBQIRL>
2019-09-19 19:34:14 +0530

    first commit....

---
+++ src/main/java/com/nt/test/CalculationTest.java
@@ -0,0 +1,25 @@
+package com.nt.test;
+
+public class CalculationTest {
+	public static int add(int a,int b)
+	{
+		return a+b;
+	}
+	public static int sub(int a,int b)
+	{
+		return a-b;
+	}
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int addition=add(5,6);
+		System.out.println(addition);
+		int sub=sub(8,6);
+		System.out.println(sub);
+		
+		
+
+	}
+
+}
